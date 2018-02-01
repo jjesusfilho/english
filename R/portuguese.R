d96a81ca68e2028e288615ff0eb30fc3981e3d0e
jjesusfilho <jjesusfilho@gmail.com>
2018-02-01 04:20:03 +0000

    new correction

--- R/portuguese.R
+++ R/portuguese.R
@@ -219,6 +219,7 @@ as.character.portuguese <- local({
           as.vector(teens[digits[1]])
       else
         trim(paste(tens[digits[2]],
+                   and(digits[1]),
                    helper(as.numeric(digits[1]))))
       else if (nDigits == 3)
         trim(paste(hundreds[digits[3]],
